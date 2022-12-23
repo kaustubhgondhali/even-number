2437fdf490c100680ed7b2ee88aacfd4f74ac42b
kaustubhgondhali <110551976+kaustubhgondhali@users.noreply.github.com>
2022-12-23 22:21:46 +0530

    Add files via upload

---
+++ TimePass.java
@@ -0,0 +1,17 @@
+package corejavademo;
+
+public class TimePass {
+	static int a;
+	
+	static void show()
+	{
+		System.out.println(a);
+	}
+	
+	public static void main(String[] args) {
+		
+		/*TimePass T = new TimePass();
+		T.show();*/
+		show();
+	}
+}
